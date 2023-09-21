966d3509b96c61fa707238bac739b245d0e4e87a
Sami Cheong <samicheong@gmail.com>
2023-09-20 22:58:16 -0500

    lecture 7 code

---
+++ lecture7/bha_exp.R
@@ -0,0 +1,13 @@
+library(daewr)
+## CRBD design (with blocking)
+mod3 <- aov( y ~ block + strain * treat, data = bha)
+summary(mod3)
+## CRD design without blocking 
+mod4 <- aov( y ~ strain * treat, data = bha)
+summary(mod4)
+
+
+
+# df_crd = 7, sigma_crd = 2.6
+# df_rcb = 8, sigma_rcb = 8.2
+RE_bha <- RE(df1 = 7, df2 = 8, mse1 = 2.6,mse2 = 8.2 )

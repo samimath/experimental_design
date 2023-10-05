8eca2d7adc650acee3dd436e037a72b7cd5bcca2
Sami Cheong <samicheong@gmail.com>
2023-10-04 21:26:32 -0500

    more code for lecture 11

--- lecture11/frf2_examples.R
+++ lecture11/frf2_examples.R
@@ -7,7 +7,17 @@ print(design)
 ## generate alias / confounding patterns
 ## place holder response vector of the same dimension as the runs 
 ## (it can be anything)
+## (.)^(k-1) denotes the design type 
+## where we want to identify confounding patterns
 y<-1:16
 aliases( lm (y~(.)^4, 
              data = design))
 
+
+### generate 2^{k-2} design
+design2 <- FrF2(nruns = 8, nfactors = 5, 
+               generators = c('AB','AC'),
+               randomize = FALSE)
+y2<-1:8
+aliases( lm (y2~(.)^3, 
+             data = design2))

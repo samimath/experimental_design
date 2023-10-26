4c1bef9de998dc4b0d1dae5a021a48da48185d9b
Sami Cheong <samicheong@gmail.com>
2023-10-25 22:16:36 -0500

    lecture 16 code

--- lecture16/comparing_confounding_patterns.R
+++ lecture16/comparing_confounding_patterns.R
@@ -6,14 +6,27 @@ library(ggplot2)
 ################################################################################
 ## plotting confounding patterns (here use (.)^2 to 
 ## indicate the inclusion of 2-factor interaction terms):
-
+## example on the fatigue life of weld-repaired castings
+## load data (contained in the BSD package provided by Lawson)
+data( PB12Des, package = "BsMD" )
+colnames(PB12Des) <- c("c11", "c10", "c9", "c8", 
+                       "G", "F", "E","D", "C", "B", "A")
+## castings factors
+castf <- PB12Des[c(11,10,9,8,7,6,5,4,3,2,1)]
+## observations
+y <- c(4.733, 4.625, 5.899, 7.0, 5.752, 5.682, 
+       6.607, 5.818,5.917, 5.863, 6.058, 4.809)
+## add observations to the design and 
+## organize data into workable format
+castf <- cbind( castf, y )
 modpb2 <- lm( y ~ (.)^2, data = subset(castf,select =-c(c8,c9,c10,c11)) )
 # get design matrix
 X_pb <-as.matrix(data.frame(model.matrix(modpb2)))
 # calculate covariance-variance matrix
 corMat<-(t(X_pb)%*%X_pb)
 ## visualize variance co-variance matrix 
-heatmap(corMat,Colv = NA, Rowv = NA, scale="column")
+heatmap(corMat,Colv = NA, Rowv = NA, 
+        scale="column",cexRow=.5)
 
 ## compare this with a fractional factorial design
 fr_design<-FrF2( nruns = 8,  nfactors = 7)
@@ -26,3 +39,17 @@ heatmap(corMat_fr,Colv = NA, Rowv = NA, scale="column")
 par(mfrow=c(1,2))
 heatmap(corMat,Colv = NA, Rowv = NA, scale="column")
 heatmap(corMat_fr,Colv = NA, Rowv = NA, scale="column")
+
+
+## compare this with a full factorial design
+full_design<-FrF2( nruns = 2^7,  nfactors = 7)
+y <-1:(2^7)
+modfull <- lm( y ~ (.)^2, data = full_design)
+X_full <-as.matrix(data.frame(model.matrix(modfull)))
+corMat_full<-(t(X_full)%*%X_full)
+heatmap(corMat_full,Colv = NA, Rowv = NA, scale="column")
+
+par(mfrow=c(1,3))
+heatmap(corMat,Colv = NA, Rowv = NA, scale="column")
+heatmap(corMat_fr,Colv = NA, Rowv = NA, scale="column")
+heatmap(corMat_full,Colv = NA, Rowv = NA, scale="column")
--- lecture16/orthogonal_array.R
+++ lecture16/orthogonal_array.R
@@ -1,14 +1,56 @@
 library("daewr")
 library('FrF2')
 library("DoE.base")
+library('AlgDesign')
 show.oas( factors = list(nlevels = c(3,2), 
                          number = c(5,2)))
 
+## smallest available array for 5 factors with 3 levels , 
+## and 2 factors with 2 levels
+## min3 here indicates the requirement to return a design 
+## with the least amount of confounding between 
+## main effects and 2-factor interactions 
 des <- oa.design(nlevels = c(3, 3, 3, 3, 3, 2, 2),
                  nruns = 36,
                  columns = "min3", 
                  randomize = TRUE, 
                  seed = 104)
-mod_test<-lm(y~(.)^2,data =des)
+y<-1:36
+mod_test<-lm(y~(.),data =des)
+## check the confounding patterns:
 X <-as.matrix(data.frame(model.matrix(mod_test)))
 XtX <-round(t(X)%*%X,2)
+heatmap(XtX,Colv = NA, Rowv = NA, scale="column")
+
+
+cand <- oa.design( nlevels = c(3, 3, 3, 3, 3, 2, 2),
+                   nruns = 36, columns = "min3", seed = 104)
+optim <- optFederov( ~ A + B + C + D + E + F + G, 
+                     cand,nRepeats = 10, nTrials = 18, criterion = "D")
+y <-1:18
+mod_test<-lm(y~A + B + C + D + E + F + G,data =optim$design)
+## check the confounding patterns:
+X <-as.matrix(data.frame(model.matrix(mod_test)))
+XtX <-round(t(X)%*%X,0)
+
+## product research example
+
+cand_chair <- oa.design(nlevels = c(4, 4, 3, 2),
+                        randomize = FALSE, 
+                        columns = 'min3',seed = 2013)
+
+optim_chair <- optFederov( ~ A + B + C + D, cand_chair,
+                           nRepeats = 10, nTrials = 12, 
+                           criterion = "D", aug = FALSE)
+
+## analyze the outcome of the experiment
+data(hardwood)
+modh <- lm(Rating ~ as.factor(Price) + Density + Guarantee + Design,
+           data = hardwood)
+anova(modh)
+
+
+modh2 <- lm(Rating ~ Price + Density + Guarantee + Design,
+           data = hardwood)
+summary(modh2)
+

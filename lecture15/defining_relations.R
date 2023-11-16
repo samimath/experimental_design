6151f3e86c514a161bc5b950a4280a2d3d562d44
Sami Cheong <samicheong@gmail.com>
2023-11-15 22:37:19 -0600

    minor cleanup

--- lecture15/defining_relations.R
+++ lecture15/defining_relations.R
@@ -2,6 +2,7 @@
 library(FrF2)
 
 make_temp_data<- function(data, factors){
+  ## force the values of the design object from FrF2 to be numeric
   temp_data<-data
   def_array<-list()
   #print(head(temp_data))
@@ -13,25 +14,15 @@ make_temp_data<- function(data, factors){
 }
 
 
-factors = c('A','B','C','D','E','F','G')
-def_array<-list()
-for(i in 1:length(factors)){
-  def_array[[i]]<-combn(x=factors,i)
-}
-
-
 def_relation <- function(data, factors){
   temp_data<-make_temp_data(data=data,factors = factors)
   #print(head(temp_data))
   def_array<-list()
   def_out<-NULL
   for(i in 2:length(factors)){
-    
     def_array[[i]]<-combn(x=factors,i)
     #print(def_array[[i]])
     for(j in 1:ncol(def_array[[i]])){
-      #print(temp_data[,def_array[[i]][,j]])
-      
       check_vec<-apply(temp_data[,def_array[[i]][,j]],1,prod)
       if(sum(check_vec)==length(check_vec)){
         out<-paste(def_array[[i]][,j],collapse="", sep="")
@@ -47,6 +38,5 @@ def_relation <- function(data, factors){
 des <- FrF2( nruns = 8,  
                   nfactors = 7, 
                   generator = c('AB','AC','BC','ABC'))
-## force the values of the design object from FrF2 to be numeric
-temp_data<-make_temp_data(data = data.frame(des),factors =c('A','B','C','D','E','F','G') )
+
 def_relation(data=data.frame(des),factors =c('A','B','C','D','E','F','G'))
